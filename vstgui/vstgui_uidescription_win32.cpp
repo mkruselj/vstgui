1e9d651b6341555c16dba7b49c16924d225807de
Mikko Saarinki <mikko.saarinki@michaelkingston.fi>
2020-11-22 13:54:31 +0200

    Fixed Visual Studio _CRT_SECURE_NO_WARNINGS define build warning

--- vstgui/vstgui_uidescription_win32.cpp
+++ vstgui/vstgui_uidescription_win32.cpp
@@ -1,8 +1,10 @@
-// This file is part of VSTGUI. It is subject to the license terms 
+// This file is part of VSTGUI. It is subject to the license terms
 // in the LICENSE file found in the top-level directory of this
 // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
 
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include "vstgui_uidescription.cpp"
 
